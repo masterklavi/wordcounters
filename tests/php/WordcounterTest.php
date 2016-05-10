8f32fd38f6113f2cddf7e690215edb99eba47612
Masterklavi <masterklavi@gmail.com>
2016-05-10 13:25:57 +0500

    added tests

---
+++ tests/php/WordcounterTest.php
@@ -0,0 +1,55 @@
+<?php
+
+class WordcounterTest extends PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        require_once '../wordcounter.php';
+    }
+
+    /**
+     * @covers ::wordcounter
+     * @dataProvider provider
+     */
+    public function testWordcounter($comment, $count, $string)
+    {
+        $this->assertEquals($count, wordcounter($string), true);
+    }
+
+    public function provider()
+    {
+        $data = [];
+
+        // table
+        foreach (scandir('data/table') as $filename)
+        {
+            if (!preg_match('#^(\d)_(\d+)\.txt$#', $filename, $matches))
+            {
+                continue;
+            }
+
+            $comment = 'table-'.$matches[1];
+            $count = $matches[2];
+            $string = file_get_contents('data/table/'.$filename);
+
+            $data[$comment] = [$comment, $count, $string];
+        }
+
+        // raw
+        foreach (scandir('data/raw') as $filename)
+        {
+            if (!preg_match('#^(\d)_(\d+)\.txt$#', $filename, $matches))
+            {
+                continue;
+            }
+
+            $comment = 'raw-'.$matches[1];
+            $count = $matches[2];
+            $string = file_get_contents('data/raw/'.$filename);
+
+            $data[$comment] = [$comment, $count, $string];
+        }
+
+        return $data;
+    }
+}
